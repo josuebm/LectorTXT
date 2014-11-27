4b404d955a5f539a1a6da0df6d6d92ee99468c5c
josuebm <josuebm@hotmail.com>
2014-11-27 21:26:05 +0100

    Init

--- app/src/main/java/com/example/josu/lectortxt/Principal.java
+++ app/src/main/java/com/example/josu/lectortxt/Principal.java
@@ -1,31 +1,21 @@
 package com.example.josu.lectortxt;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.net.Uri;
-import android.opengl.Visibility;
 import android.os.Bundle;
 import android.os.Environment;
-import android.util.Xml;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.xmlpull.v1.XmlSerializer;
-
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
 
 
